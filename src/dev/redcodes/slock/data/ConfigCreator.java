e9830167cf68c66a79afd3d970a8b4975680088f
David <david.kesselmann@gmail.com>
2021-05-24 21:28:22 +0200

    Fixed grammar

--- src/dev/redcodes/slock/data/ConfigCreator.java
+++ src/dev/redcodes/slock/data/ConfigCreator.java
@@ -12,14 +12,14 @@ public class ConfigCreator {
 		config.saveConfig();
 		
 		FileConfig msgConfig = new FileConfig("Slock", "messages.yml");
-		msgConfig.options().header("Here you can edit all of the Messages for the Plugin\n\nYou can also use Color Codes (https://www.spigotmc.org/attachments/example2-png.323205/)");
+		msgConfig.options().header("Here you can edit all the Messages for the Plugin\n\nYou can also use Color Codes (https://www.spigotmc.org/attachments/example2-png.323205/)");
 		msgConfig.addDefault("Prefix", "[&aSlock&r]");
 		msgConfig.addDefault("Messages.SlockOn", "&cThe Server is now locked.");
 		msgConfig.addDefault("Messages.SlockOff", "&aThe Server is now unlocked.");
 		msgConfig.addDefault("Messages.Kick", "&4The Server was locked by an administrator.");
 		msgConfig.addDefault("Messages.KickOnJoin", "&4The Server is locked.");
 		msgConfig.addDefault("Messages.Bypass", "&cThe Server is currently locked. &aYou bypassed the lock.");
-		msgConfig.addDefault("Messages.Errors.NoPermission", "&4You dont have Permission to run this command.");
+		msgConfig.addDefault("Messages.Errors.NoPermission", "&4You don't have Permission to run this command.");
 		msgConfig.addDefault("Messages.Errors.FalseArguments", "&bDid you mean &n/slock [on/off]&r&b?");
 		msgConfig.addDefault("Messages.Errors.SlockAlreadyOn", "&4The Server is already locked.");
 		msgConfig.addDefault("Messages.Errors.SlockAlreadyOff","&4The Server is already unlocked.");
