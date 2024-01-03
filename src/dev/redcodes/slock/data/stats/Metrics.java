4f448eb592a7d5bd232e5f692932de9566d86016
David Kesselmann <david@redcodes.dev>
2024-01-03 21:04:13 +0100

    Finished 1.3.2

--- src/dev/redcodes/slock/data/stats/Metrics.java
+++ src/dev/redcodes/slock/data/stats/Metrics.java
@@ -21,7 +21,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Method;
-import java.net.URL;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collection;
@@ -310,7 +310,7 @@ public class Metrics {
         infoLogger.accept("Sent bStats metrics data: " + data.toString());
       }
       String url = String.format(REPORT_URL, platform);
-      HttpsURLConnection connection = (HttpsURLConnection) new URL(url).openConnection();
+      HttpsURLConnection connection = (HttpsURLConnection) new URI(url).toURL().openConnection();
       // Compress the data to save bandwidth
       byte[] compressedData = compress(data.toString());
       connection.setRequestMethod("POST");
